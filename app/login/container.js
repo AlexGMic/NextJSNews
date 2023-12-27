f3710f27acbad47eb03d31b3c64cd59f621e1713
AlexGMic <alemkgbremichael@gmail.com>
2023-12-27 23:18:27 +0300

    fifteenth commit

--- app/login/container.js
+++ app/login/container.js
@@ -13,7 +13,7 @@ export default function LoginContainer() {
         <div className="w-[50%] max-[1100px]:hidden">
           <Image
             className="w-full object-cover"
-            src={process?.env?.NEXT_PUBLIC_ARTWORK_URL}
+            src={"/artwork-4.jpg"}
             priority={true}
             alt="News Image"
             width={500}
