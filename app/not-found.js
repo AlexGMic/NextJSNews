0f65f36a71a521b3d592eca6d46fa0e395707764
AlexGMic <alemkgbremichael@gmail.com>
2023-12-27 23:10:12 +0300

    fourteenth commit

--- app/not-found.js
+++ app/not-found.js
@@ -8,7 +8,7 @@ export default function Error() {
           <p className="text-5xl font-semibold my-4 mb-16 text-[#0D5C63]">Abyssinia News</p>
         </div>
         <div className="w-full flex flex-col items-center justify-center gap-8 text-[#878787]">
-          <p className="text-4xl font-semibold">404 Page</p>
+          <p className="text-4xl font-semibold">404 Error Page</p>
           <p className="text-3xl font-semibold">Page not found</p>
           <Link href={'/'} className="bg-[#0D5C63] text-white text-2xl font-semibold px-4 py-2 rounded-3xl hover:bg-transparent hover:text-[#0D5C63] transition duration-500 border border-[#0D5C63]">Home Page</Link>
         </div>
