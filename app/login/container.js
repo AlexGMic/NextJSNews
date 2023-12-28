57e574a5fbc33427fb348e81967eb51ad6e59249
AlexGMic <alemkgbremichael@gmail.com>
2023-12-28 13:34:43 +0300

    thirtyfive commit

--- app/login/container.js
+++ app/login/container.js
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import Login from "./login";
 import Signup from "./signup";
 import Image from "next/image";
@@ -12,16 +11,15 @@ export default function LoginContainer() {
     <div className="w-[100%] h-[100vh] min-h-[100vh] flex items-center">
       <div className="w-[90%] h-[100vh] mx-auto flex items-center max-[1400px]:w-full">
         <div className="w-[50%] max-[1100px]:hidden">
-          <Link href={"/login"} as={"image"}>
-            <Image
-              src={`${process?.env?.NEXTAUTH_URL}/MediaFolders/artwork-4.jpg`}
-              className="w-full object-cover"
-              priority={true}
-              alt="News Image"
-              width={500}
-              height={500}
-            />
-          </Link>
+          <Image
+            src="/MediaFolders/artwork-4.jpg"
+            className="w-full object-cover"
+            alt="News Image"
+            width={500}
+            height={500}
+            placeholder="blur"
+            blurDataURL="/MediaFolders/artwork-4.jpg"
+          />
         </div>
         {loginState ? (
           <Login setLoginState={setLoginState} />
