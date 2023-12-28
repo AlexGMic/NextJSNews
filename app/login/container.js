eed521a47fcd83ee4e6ddcddf290f3f458020c29
AlexGMic <alemkgbremichael@gmail.com>
2023-12-28 12:04:18 +0300

    twentyseven commit

--- app/login/container.js
+++ app/login/container.js
@@ -6,14 +6,13 @@ import Image from "next/image";
 import { useState } from "react";
 
 export default function LoginContainer() {
-  console.log(process.env.NEXT_PUBLIC_ARTWORK_URL, "User Image")
   const [loginState, setLoginState] = useState(true);
   return (
     <div className="w-[100%] h-[100vh] min-h-[100vh] flex items-center">
       <div className="w-[90%] h-[100vh] mx-auto flex items-center max-[1400px]:w-full">
         <div className="w-[50%] max-[1100px]:hidden">
           <Image
-            src={"/artwork-4.jpg"}
+            src="/public/artwork-4.jpg"
             className="w-full object-cover"
             priority={true}
             alt="News Image"
