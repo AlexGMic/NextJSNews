be719264885c85e7390f9869fe8905029e9aa45e
AlexGMic <alemkgbremichael@gmail.com>
2023-12-28 14:17:59 +0300

    fourtythree commit

--- app/login/container.js
+++ app/login/container.js
@@ -10,16 +10,16 @@ export default function LoginContainer() {
   return (
     <div className="w-[100%] h-[100vh] min-h-[100vh] flex items-center">
       <div className="w-[90%] h-[100vh] mx-auto flex items-center max-[1400px]:w-full">
-        <div className="w-[50%] max-[1100px]:hidden">
+        {/* <div className="w-[50%] max-[1100px]:hidden">
           <Image
-            src={process.env.NEXT_PUBLIC_ARTWORK_URL}
+            src={`${process?.env?.NEXT_PUBLIC_NEXTAUTH_URL}/MediaFolders/artwork-4.jpg`}
             className="w-full object-cover"
             priority={true}
             alt="News Image"
             width={500}
             height={500}
           />
-        </div>
+        </div> */}
         {loginState ? (
           <Login setLoginState={setLoginState} />
         ) : (
